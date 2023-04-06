de0ca7b4c4fa067eec95ae282e9f05ec6f59c795
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-04-06 19:08:38 +0300

    Add search recipe function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ const recipeForm = document.querySelector("#recipe-form");
 const results = document.querySelector("#results");
 
 
-
-
-
-
-
-
-
+// Functions:
 document.addEventListener("DOMContentLoaded", () => {
     recipeForm.addEventListener("submit", searchFoods);
  });
@@ -17,10 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
  const searchFoods = (e) => {
     e.preventDefault();
     const searchParameter = document.querySelector('#search').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchParameter}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchParameter}`)
     .then(resp => resp.json())
     .then(data => { renderFoods(data.meals) })
-    // .catch(error => { alert("There seems to be an error!") });
+    .catch(error => { alert("Recipes could not be found!") });
 
  }
 
@@ -29,22 +23,22 @@ function renderFoods(meals = []){
 
         const html = `
             <div id="FoodCard" data-id="${meal.idMeal}" class="card">
-                <img id="FoodImage" src="${meal.strMealThumb}" alt="Food Image">
+                <img id="FoodImage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
                 <div class="container">
                     <h4 id="FoodName">${meal.strMeal}</h4>
                     <div>
-                    <button class="allbtns" id="foodRecipe">See Recipe</button>
+                        <button class="allbtns" id="foodRecipe">See Recipe</button>
                     </div>
                     <br>
                     <div>
-                    <button class="allbtns" id="nutritionDetails">Nutrition Details</button>
+                    <a href="${meal.strYoutube}javascript:alert('Video not Found.');"><button class="allbtns" id="nutritionDetails">Recipe Video Guide</button></a>
                     </div>
                     <br>
                 </div>
             </div>
         `;
         results.insertAdjacentHTML('beforeend',html)
-      })
-
+    })
 }
+
 
