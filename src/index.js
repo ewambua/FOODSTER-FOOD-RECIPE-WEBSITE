a4ee0f8c6250c50e396bebb1f7d7847c784d5dd1
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-04-07 19:55:51 +0300

    final Commit

--- src/index.js
+++ src/index.js
@@ -22,16 +22,21 @@ function renderFoods(meals = []){
     meals.forEach(meal => {
 
         const html = `
-            <div id="FoodCard" data-id="${meal.idMeal}" class="card">
+            <div id="FoodCard" data-id="${meal.idMeal}" class="card ">
                 <img id="FoodImage" src="${meal.strMealThumb}" alt="${meal.strMeal}">
                 <div class="container">
-                    <h4 id="FoodName">${meal.strMeal}</h4>
+                <h4 id="FoodName">${meal.strMeal}</h4>
                     <div>
-                        <button class="allbtns" id="foodRecipe">See Recipe</button>
+                        <button class="allbtns" id="foodRecipe" data-toggle="collapse" data-target="#demo">See Recipe</button>
+                            <div id="demo" class="collapse bg-light bg-gradient">
+                                <h4>Instructions for coocking (${meal.strMeal}) </h4>
+                                <hr>
+                                <p>${meal.strInstructions}</p>
+                            </div>
                     </div>
                     <br>
                     <div>
-                    <a href="${meal.strYoutube}javascript:alert('Video not Found.');"><button class="allbtns" id="nutritionDetails">Recipe Video Guide</button></a>
+                    <a href="${meal.strYoutube}javascript:alert('Video not Found.');"><button class="allbtns">Recipe Video Guide</button></a>
                     </div>
                     <br>
                 </div>
